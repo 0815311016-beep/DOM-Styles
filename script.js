627ecb6c6085e5063d00fe3a3b04df68b2f49939
0815311016-beep <0815311016@s.tusd1.org>
2026-03-05 16:25:51 +0000

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log("Script started");
+
+function changeStyles() {
+    console.log("click");
+
+    let p = document.getElementById("text");
+    p.style.margin = "50px";
+    p.style.color = "pink";
+    p.style.fontSize = "40px";
+    p.style.textAlign = "center";
+}
+
